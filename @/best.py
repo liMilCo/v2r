b1007b0521cc4a2e952664919d489bcbd88c59e7
liMil <91998494+liMilCo@users.noreply.github.com>
2025-08-12 19:36:18 +0330

    liMil Updated✅

---
+++ @/best.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+
+# Define a fixed value
+TIMEOUT = 15  # seconds
+Best_URL = "https://btc.liwil.ir/pages"
+
+print(f"\nUpdate Best Configs ...")
+output_folder = os.path.join(os.path.dirname(__file__), "..")
+best_file = os.path.join(output_folder, 'best.txt')
+
+print(f"Remove {best_file}")
+if os.path.exists(best_file):
+    os.remove(best_file)
+
+print(f"Get Best Configs ...")
+
+response = requests.get(Best_URL, timeout=TIMEOUT)
+best_text = response.text
+
+with open(best_file, "w", encoding="utf-8") as f:
+    f.write(best_text)
+
+print(f"Update Best successfully!")    
